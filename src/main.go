016257e39065d0b7a4cda655439fc55e21a3b24f
Danial Mobini <danialmobinidh81@gmail.com>
2025-09-08 19:36:19 +0330

    Refactor code structure for improved readability and maintainability

--- src/main.go
+++ src/main.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/danialmd81/my-subscribtion/all"
 	"github.com/danialmd81/my-subscribtion/subs"
+	"github.com/danialmd81/my-subscribtion/surfboard"
 )
 
 func main() {
 	// telegram.Run()
 	subs.Run()
 	all.Run()
+	fmt.Println(surfboard.GenerateSSConfig("all/ss.txt", "surfboard/ss.conf"))
+	fmt.Println(surfboard.GenerateVMessConfig("all/vmess.txt", "surfboard/vmess.conf"))
+	fmt.Println(surfboard.GenerateHysteriaConfig("all/hysteria.txt", "surfboard/hysteria.conf"))
+
 }
