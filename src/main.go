17ceb4450d70113944e06dc4da3cb4ad0f512602
Danial Mobini <danialmobinidh81@gmail.com>
2026-01-28 11:55:16 +0330

    Update vmess configuration in telegram/vmess.txt to include new server details and remove outdated entries

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"github.com/danialmd81/my-subscribtion/all"
 	"github.com/danialmd81/my-subscribtion/subs"
 	"github.com/danialmd81/my-subscribtion/telegram"
 )
@@ -9,7 +8,7 @@ import (
 func main() {
 	telegram.Run()
 	subs.Run()
-	all.Run()
+	// all.Run()
 	// fmt.Println(surfboard.GenerateSSConfig("all/ss.txt", "surfboard/ss.conf"))
 	// fmt.Println(surfboard.GenerateVMessConfig("all/vmess.txt", "surfboard/vmess.conf"))
 	// fmt.Println(surfboard.GenerateHysteriaConfig("all/hysteria.txt", "surfboard/hysteria.conf"))
