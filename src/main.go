1cb9917a1e9c2e299a5249ba0ee6ff5556e3b03e
Danial Mobini <danialmobinidh81@gmail.com>
2026-01-30 19:40:21 +0330

    Refactor Surfboard configuration files: - Updated vmess configuration in `vmess.txt` with new server details. - Removed outdated README files from `1_subscription`, `2_general`, `3_group`, `4_rule`, and root directory to streamline documentation.

--- src/main.go
+++ src/main.go
@@ -1,16 +1,30 @@
 package main
 
 import (
+	"os"
+
 	"github.com/danialmd81/my-subscribtion/subs"
 	"github.com/danialmd81/my-subscribtion/telegram"
 )
 
 func main() {
+	// Save current proxy settings
+	origHTTPProxy := os.Getenv("HTTP_PROXY")
+	origHTTPSProxy := os.Getenv("HTTPS_PROXY")
+
+	// Set proxy
+	os.Setenv("HTTP_PROXY", "http://127.0.0.1:2080")
+	os.Setenv("HTTPS_PROXY", "http://127.0.0.1:2080")
+
+	// Run telegram with proxy
 	telegram.Run()
+
+	// Restore original proxy settings
+	os.Setenv("HTTP_PROXY", origHTTPProxy)
+	os.Setenv("HTTPS_PROXY", origHTTPSProxy)
+
+	// Continue with other services
 	subs.Run()
 	// all.Run()
-	// fmt.Println(surfboard.GenerateSSConfig("all/ss.txt", "surfboard/ss.conf"))
-	// fmt.Println(surfboard.GenerateVMessConfig("all/vmess.txt", "surfboard/vmess.conf"))
-	// fmt.Println(surfboard.GenerateHysteriaConfig("all/hysteria.txt", "surfboard/hysteria.conf"))
 
 }
